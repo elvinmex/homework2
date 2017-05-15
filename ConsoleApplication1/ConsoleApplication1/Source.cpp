1a0b59d88353f72e560cf35090a701fe0682db97
elvinmex <elvin.mex@bk.ru>
2017-05-15 11:41:01 +0400

    changes2

--- ConsoleApplication1/ConsoleApplication1/Source.cpp
+++ ConsoleApplication1/ConsoleApplication1/Source.cpp
@@ -1,1 +1,68 @@
-//test
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+void main()
+{
+	int key = getch();
+	int arr[4][4] = { 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,0 };
+	while (true)
+	{
+		for (int i = 0; i < 4; i++)
+		{
+			for (int j = 0; j < 4; j++)
+			{
+				cout << arr[i][j] << "\t";
+				if (key == 224)
+				{
+					arr[i][j] = arr[i][j - 1];
+					arr[i][j - 1] = 0;
+				}
+				if (key == 77)
+				{
+					arr[i][j] = arr[i + 1][j];
+					arr[i + 1][j] = 0;
+				}
+				if (key == 72)
+				{
+					arr[i][j] = arr[i - 1][j];
+					arr[i - 1][j] = 0;
+				}
+				if (key == 80)
+				{
+					arr[i][j] = arr[i][j + 1];
+					arr[i][j + 1] = 0;
+				}
+			}
+			cout << "\n\n\n" << endl;
+		}
+		system("cls");
+	}
+	//if (key == 224)
+	//{
+	//	arr[i][j] = arr[i][j - 1];
+	//	arr[i][j - 1] = 0;
+	//}
+	//if (key == 77)
+	//{
+	//	arr[i][j] = arr[i+1][j];
+	//	arr[i+1][j] = 0;
+	//}
+	//if (key == 72)
+	//{
+	//	arr[i][j] = arr[i-1][j];
+	//	arr[i-1][j] = 0;
+	//}
+	//if (key == 80)
+	//{
+	//	arr[i][j] = arr[i][j + 1];
+	//	arr[i][j + 1] = 0;
+	//}
+	//224 verx
+	//77 napravo
+	//72 nalevo
+	//80 vniz
+	
+}
+
+

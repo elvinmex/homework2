7288b0cb2b17bb39c0bd9faec8ebfac5bc4d71ff
elvinmex <elvin.mex@bk.ru>
2017-05-16 23:59:49 +0400

    first

--- ConsoleApplication1/ConsoleApplication1/Source.cpp
+++ ConsoleApplication1/ConsoleApplication1/Source.cpp
@@ -4,40 +4,74 @@ using namespace std;
 
 void main()
 {
-	int key = getch();
+	int i = 2, j = 2;
 	int arr[4][4] = { 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,0 };
-	while (true)
+	for (int i = 0; i < 4; i++)
 	{
+		for (int j = 0; j < 4; j++)
+		{
+			cout << arr[i][j] << "\t";
+		}
+		cout << "\n\n\n" << endl;
+	}
+	int key = getch();
+	if (key == 224)
+		{
+			arr[i][j] = arr[i][j - 1];
+			arr[i][j - 1] = 0;
+		}
+		if (key == 77)
+		{
+			arr[i][j] = arr[i + 1][j];
+			arr[i + 1][j] = 0;
+		}
+		if (key == 72)
+			{
+				arr[i][j] = arr[i - 1][j];
+				arr[i - 1][j] = 0;
+			}
+		if (key == 80)
+			{
+				arr[i][j] = arr[i][j + 1];
+				arr[i][j + 1] = 0;
+			}
 		for (int i = 0; i < 4; i++)
 		{
 			for (int j = 0; j < 4; j++)
 			{
 				cout << arr[i][j] << "\t";
-				if (key == 224)
-				{
-					arr[i][j] = arr[i][j - 1];
-					arr[i][j - 1] = 0;
-				}
-				if (key == 77)
-				{
-					arr[i][j] = arr[i + 1][j];
-					arr[i + 1][j] = 0;
-				}
-				if (key == 72)
-				{
-					arr[i][j] = arr[i - 1][j];
-					arr[i - 1][j] = 0;
-				}
-				if (key == 80)
-				{
-					arr[i][j] = arr[i][j + 1];
-					arr[i][j + 1] = 0;
-				}
 			}
 			cout << "\n\n\n" << endl;
 		}
-		system("cls");
-	}
+	//while (true)
+	//{
+	//	//risovaniye polya
+
+	//	system("cls");
+
+	//	if (key == 224)
+	//		{
+	//			arr[i][j] = arr[i][j - 1];
+	//			arr[i][j - 1] = 0;
+	//		}
+	//		if (key == 77)
+	//		{
+	//			arr[i][j] = arr[i + 1][j];
+	//			arr[i + 1][j] = 0;
+	//		}
+	//		if (key == 72)
+	//			{
+	//				arr[i][j] = arr[i - 1][j];
+	//				arr[i - 1][j] = 0;
+	//			}
+	//		if (key == 80)
+	//			{
+	//				arr[i][j] = arr[i][j + 1];
+	//				arr[i][j + 1] = 0;
+	//			}
+
+	//	system("cls");
+	//}
 	//if (key == 224)
 	//{
 	//	arr[i][j] = arr[i][j - 1];

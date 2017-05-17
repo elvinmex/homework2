e17a102ead4c10e15f4e941bb43c43540ce77d04
asdasd <asdasd>
2017-05-17 11:51:07 +0400

    asd

--- ConsoleApplication1/ConsoleApplication1/Source.cpp
+++ ConsoleApplication1/ConsoleApplication1/Source.cpp
@@ -1,102 +1,120 @@
+//#include <iostream>
+//#include <conio.h>
+//using namespace std;
+//int k, u;
+//
+//void naxojdeniye_nulya(int arr[4][4]);
+//void main()
+//{
+//	const int z = 4, y = 4;
+//	int arr[z][y] = { 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,0 };
+//	for (int i = 0; i < 4; i++)
+//	{
+//		for (int j = 0; j < 4; j++)
+//		{
+//			cout << arr[i][j] << "\t";
+//		}
+//		cout << "\n\n\n" << endl;
+//	}
+//	naxojdeniye_nulya(arr);
+//	while (true)
+//	{
+//		int key = getch();
+//
+//		//Initializaciya klavish
+//		if (key == 75)
+//		{
+//			arr[k][u] = arr[k][u - 1];
+//			arr[k][u - 1] = 0;
+//			u--;
+//		}
+//		else if (key == 77)
+//		{
+//			arr[k][u] = arr[k][u + 1];
+//			arr[k][u + 1] = 0;
+//			u++;
+//
+//		}
+//		else if (key == 72)
+//		{
+//			arr[k][u] = arr[k - 1][u];
+//			arr[k - 1][u] = 0;
+//			k--;
+//		}
+//		else if (key == 80)
+//		{
+//			arr[k][u] = arr[k + 1][u];
+//			arr[k + 1][u] = 0;
+//			k++;
+//		}
+//		////////////////////
+//		////////////////////
+//
+//		system("cls");
+//
+//		for (int i = 0; i < 4; i++)
+//		{
+//			for (int j = 0; j < 4; j++)
+//			{
+//				cout << arr[i][j] << "\t";
+//			}
+//			cout << "\n\n\n" << endl;
+//		}
+//	}
+//
+//}
+//
+//
+//void naxojdeniye_nulya(int arr[4][4])
+//{
+//	//Naxojdeniye nulya
+//	for (int i = 0; i < 4; i++)
+//	{
+//		for (int j = 0; j < 4; j++)
+//		{
+//			if (arr[i][j] == 0)
+//			{
+//				k = i;
+//				u = j;
+//			}
+//		}
+//	}
+//	///////////////////
+//	///////////////////
+//}
+//
+//
+//
+
+#include <string>
 #include <iostream>
 #include <conio.h>
 using namespace std;
+void morze()
+{
+	int cet = 0;
+	int key = getch();
+	const int size = 28;
+	string arr_morze[size] = { ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.." };
+	int arr_norm[size] = {};
+	for (int i = 0; i < size; i++)
+	{
+		arr_norm[i] = cet;
+		cet++;
+	}
+	while (true)
+	{
+		for (int i = 0; i < size; i++)
+		{
+			if (key == arr_norm[i])
+			{
+				cout << arr_morze[i] << endl;
+			}
+		}
+	}
+}
 
 void main()
 {
-	int i = 2, j = 2;
-	int arr[4][4] = { 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,0 };
-	for (int i = 0; i < 4; i++)
-	{
-		for (int j = 0; j < 4; j++)
-		{
-			cout << arr[i][j] << "\t";
-		}
-		cout << "\n\n\n" << endl;
-	}
-	int key = getch();
-	if (key == 224)
-		{
-			arr[i][j] = arr[i][j - 1];
-			arr[i][j - 1] = 0;
-		}
-		if (key == 77)
-		{
-			arr[i][j] = arr[i + 1][j];
-			arr[i + 1][j] = 0;
-		}
-		if (key == 72)
-			{
-				arr[i][j] = arr[i - 1][j];
-				arr[i - 1][j] = 0;
-			}
-		if (key == 80)
-			{
-				arr[i][j] = arr[i][j + 1];
-				arr[i][j + 1] = 0;
-			}
-		for (int i = 0; i < 4; i++)
-		{
-			for (int j = 0; j < 4; j++)
-			{
-				cout << arr[i][j] << "\t";
-			}
-			cout << "\n\n\n" << endl;
-		}
-	//while (true)
-	//{
-	//	//risovaniye polya
-
-	//	system("cls");
-
-	//	if (key == 224)
-	//		{
-	//			arr[i][j] = arr[i][j - 1];
-	//			arr[i][j - 1] = 0;
-	//		}
-	//		if (key == 77)
-	//		{
-	//			arr[i][j] = arr[i + 1][j];
-	//			arr[i + 1][j] = 0;
-	//		}
-	//		if (key == 72)
-	//			{
-	//				arr[i][j] = arr[i - 1][j];
-	//				arr[i - 1][j] = 0;
-	//			}
-	//		if (key == 80)
-	//			{
-	//				arr[i][j] = arr[i][j + 1];
-	//				arr[i][j + 1] = 0;
-	//			}
-
-	//	system("cls");
-	//}
-	//if (key == 224)
-	//{
-	//	arr[i][j] = arr[i][j - 1];
-	//	arr[i][j - 1] = 0;
-	//}
-	//if (key == 77)
-	//{
-	//	arr[i][j] = arr[i+1][j];
-	//	arr[i+1][j] = 0;
-	//}
-	//if (key == 72)
-	//{
-	//	arr[i][j] = arr[i-1][j];
-	//	arr[i-1][j] = 0;
-	//}
-	//if (key == 80)
-	//{
-	//	arr[i][j] = arr[i][j + 1];
-	//	arr[i][j + 1] = 0;
-	//}
-	//224 verx
-	//77 napravo
-	//72 nalevo
-	//80 vniz
-	
+	void morze();
 }
-
-
